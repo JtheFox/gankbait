298ba8f41f0599582466e7bdc2c347f4e0d12ece
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-09-02 16:06:59 -0400

    Create auth code grant flow

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const $ = (sel) => document.querySelector(sel);
 const onClick = (el, cb) => el.addEventListener('click', cb)
 
-const oauthURL = 'https://discord.com/api/oauth2/authorize?client_id=869983919578173482&redirect_uri=http%3A%2F%2Flocalhost%3A3000&response_type=token&scope=identify'
+const oauthURL = 'https://discord.com/api/oauth2/authorize?client_id=869983919578173482&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin&response_type=code&scope=identify'
 
 window.onload = () => {
   // const fragment = new URLSearchParams(window.location.hash.slice(1));
--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,47 @@
 const router = require('express').Router();
-const { request } = require('undici');
+const axios = require('axios');
 require('dotenv');
 
-router.get('/', async ({ query }, res) => {
-  const { code } = query;
-  console.log(code)
-
-  if (code) {
-    try {
-      const tokenResponseData = await request('https://discord.com/api/oauth2/token', {
-        method: 'POST',
-        body: new URLSearchParams({
-          client_id: process.env.DISCORD_CLIENT_ID,
-          client_secret: process.env.DISCORD_CLIENT_SECRET,
-          code,
-          grant_type: 'authorization_code',
-          redirect_uri: `http://localhost:3000`,
-          scope: 'identify',
-        }),
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      });
-
-      const oauthData = await tokenResponseData.body.json();
-      console.log(oauthData);
-    } catch (error) {
-      // NOTE: An unauthorized token will not throw an error
-      // tokenResponseData.statusCode will be 401
-      console.error(error);
-    }
-  }
+router.get('/', async (req, res) => {
   res.render('index');
 });
 
-router.get('/login', (req, res) => {
+router.get('/login', async (req, res) => {
+  try {
+    const { code } = req.query;
+    if (!code) return res.status(401).json({ message: "Invalid authorization" });
 
+    const tokenResponseData = await axios.post(
+      'https://discord.com/api/oauth2/token',
+      new URLSearchParams({
+        client_id: process.env.DISCORD_CLIENT_ID,
+        client_secret: process.env.DISCORD_CLIENT_SECRET,
+        code: code,
+        grant_type: 'authorization_code',
+        redirect_uri: `http://localhost:3000/login`,
+        scope: 'identify',
+      }),
+      {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+      }
+    );
+
+    const { data } = tokenResponseData;
+    const userResult = await axios.get('https://discord.com/api/users/@me', {
+      headers: {
+        authorization: `${data.token_type} ${data.access_token}`,
+      },
+    });
+
+    console.log(userResult.data);
+
+    return res.redirect('/');
+  } catch (err) {
+    // NOTE: An unauthorized token will not throw an error
+    // tokenResponseData.statusCode will be 401
+    console.error(err.stack);
+    return res.status(500).json({ message: "An internal server error occurred" });
+  }
 });
 
 router.get('*', (req, res) => res.redirect('/'));
