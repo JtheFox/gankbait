959cd3195bf025bce88e6df4ab9ebad8c3c82f1f
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-09-08 14:19:31 -0400

    Refactor

--- routes/index.js
+++ routes/index.js
@@ -1,80 +1,83 @@
 const router = require('express').Router();
 const axios = require('axios');
-const { generateToken, authenticateToken, checkToken } = require('../utils/auth');
+const { generateToken, checkToken } = require('../utils/auth');
 const { User } = require('../models');
 
 const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
 router.get('/', checkToken, async (req, res) => {
-  const oauthURL = process.env.NODE_ENV === 'production' ?
-    'https://discord.com/api/oauth2/authorize?client_id=1016791443739779072&redirect_uri=https%3A%2F%2Fgankbait.jthefox.com%2Flogin&response_type=code&scope=identify ' :
-    'https://discord.com/api/oauth2/authorize?client_id=1016791443739779072&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin&response_type=code&scope=identify'
-  if (!req.userData) return res.render('index', { oauthURL, loggedIn: false });
-  const { id, username, avatar } = req.userData;
-  const dbUser = await User.get({ id });
-  const user = {
-    ...dbUser,
-    username,
-    avatar: `https://cdn.discordapp.com/avatars/${id}/${avatar}`,
-  }
-  return res.render('dashboard', { user, loggedIn: true });
-});
+  const { code, action } = req.query;
+  const redirectURL = req.protocol + '://' + req.get('host') + '/';
+  const oauthURL =`https://discord.com/api/oauth2/authorize?client_id=1016791443739779072&redirect_uri=${encodeURIComponent(redirectURL)}&response_type=code&scope=identify`;
 
-router.get('/login', checkToken, async (req, res) => {
-  if (req.userData || req.query.error === 'access_denied') return res.redirect('/')
-  
   try {
-    const { code } = req.query;
-    if (!code) return res.sendStatus(401)
-
-    const tokenResponseData = await axios.post(
-      'https://discord.com/api/oauth2/token',
-      new URLSearchParams({
-        client_id: process.env.DISCORD_CLIENT_ID,
-        client_secret: process.env.DISCORD_CLIENT_SECRET,
-        code: code,
-        grant_type: 'authorization_code',
-        redirect_uri: process.env.NODE_ENV === 'production' ? `https://gankbait.jthefox.com/login` : 'http://localhost:3000/login',
-        scope: 'identify',
-      }),
-      {
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-      }
-    );
-
-    const { data } = tokenResponseData;
-    const user = await axios.get('https://discord.com/api/users/@me', {
-      headers: {
-        authorization: `${data.token_type} ${data.access_token}`,
-      },
-    });
-    const token = generateToken(user.data);
-
-    try {
-      await User.create({ "id": user.data.id });
-      console.log('New user has logged in');
-    } catch {
-      console.log('Existing user has logged in');
+    if (req.userData && action === 'logout') {
+      return res
+        .clearCookie('access_token')
+        .status(200)
+        .redirect('/');
     }
 
-    return res
-      .cookie('access_token', token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-      })
-      .redirect('/')
+    if (!req.userData && !code || req.query.error === 'access_denied') {
+      console.info('Displaying login page');
+      return res.render('index', { oauthURL, loggedIn: false });
+    }
+
+    if (!req.userData && code) {
+      const tokenResponseData = await axios.post(
+        'https://discord.com/api/oauth2/token',
+        new URLSearchParams({
+          client_id: process.env.DISCORD_CLIENT_ID,
+          client_secret: process.env.DISCORD_CLIENT_SECRET,
+          code: code,
+          grant_type: 'authorization_code',
+          redirect_uri: redirectURL,
+        }),
+        {
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+        }
+      );
+
+      const { data } = tokenResponseData;
+      const user = await axios.get('https://discord.com/api/users/@me', {
+        headers: {
+          authorization: `${data.token_type} ${data.access_token}`,
+        },
+      });
+      const token = generateToken(user.data);
+
+      try {
+        await User.create({ "id": user.data.id });
+        console.log('New user has logged in');
+      } catch {
+        console.log('Existing user has logged in');
+      }
+
+      return res
+        .cookie('access_token', token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+        })
+        .status(200)
+        .redirect('/');
+    }
+
+    const { id, username, avatar } = req.userData;
+    const dbUser = await User.get({ id });
+    const user = {
+      ...dbUser,
+      username,
+      avatar: `https://cdn.discordapp.com/avatars/${id}/${avatar}`,
+    }
+
+    console.info('Welcome', id)
+    return res.render('dashboard', { user, loggedIn: true });
   } catch (err) {
     console.error(err.stack || err.response.data || err);
-    return res.status(500).json({ message: 'An internal server error occurred' });
+    if (err.response?.data?.error_description === 'Invalid "redirect_uri" in request.') console.info(redirectURL, oauthURL);
+    return res.redirect('/');
   }
-});
-
-router.get('/logout', authenticateToken, (req, res) => {
-  return res
-    .clearCookie('access_token')
-    .status(200)
-    .redirect('/')
 });
 
 router.get('*', (req, res) => res.redirect('/'));
