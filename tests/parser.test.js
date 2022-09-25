e13e12ec68b8b13efc4a33ebadc3f89137943eb1
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-09-25 15:03:55 -0400

    Initial tests setup

---
+++ tests/parser.test.js
@@ -0,0 +1,4 @@
+const { parseMatchData, parseTimelineData } = require('../utils/parser');
+const match = require('./exampleData/matchData');
+const timeline = require('./exampleData/timelineData');
+const summonerId = 'LxX5R76anqiiD9zGBEJtd2Lzgg2IQUFWu6XAz6CaRE6L7ro';
